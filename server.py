f8ec6accc6fb3ff293c418b7655b62f5e9372aca
romanTechDev <83161900+romanTechDev@users.noreply.github.com>
2021-08-06 21:48:27 +0300

    final server flask code

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template, flash, redirect, Markup
+import subprocess
 # from flask_login import LoginManager, login_user, login_required, UserMixin
 
 from Scripts.authentication import authentication_user
+from Scripts import get_all_about_disks
 
 # configuration
 DEBUG = True
@@ -9,6 +11,13 @@ DEBUG = True
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'jijwiajdiwaj9ji2j2hhnwa989jkmxzlpppwq2'
+
+all_disks_name_array = []
+all_disks_size_array = []
+all_disks_type_array = []
+all_disks_mountpoint_array = []
+all_disks_priority_array = []
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -17,7 +26,12 @@ app.config.from_object(__name__)
 def authentication():
     if request.method == "POST":
         try:
-            if authentication_user(request.form['login_input'],request.form['password_input']):
+            if authentication_user(request.form['login_input'], request.form['password_input']):
+                all_disks_name_array = get_all_about_disks.all_disks_name_array
+                all_disks_size_array = get_all_about_disks.all_disks_size_array
+                all_disks_type_array = get_all_about_disks.all_disks_type_array
+                all_disks_mountpoint_array = get_all_about_disks.all_disks_mountpoint_array
+                all_disks_priority_array = get_all_about_disks.all_disks_priority_array
                 return redirect('workspace.html')
             else:
                 flash('Неккоректные данные !', category='error')
@@ -27,3 +41,51 @@ def authentication():
             return render_template('authentication.html')
     else:
         return render_template('authentication.html')
+
+
+def mount_disk(disk_name,mountpoint):
+    mount_cmd = f'sudo mount -v {disk_name} {mountpoint}'
+    subprocess.run(mount_cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    pass
+
+
+def umount_disk(mountpoint):
+    umount_cmd = 'sudo umount ' + mountpoint
+    subprocess.run(umount_cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    pass
+
+
+def format_disk(mountpoint):
+    format_cmd = 'scrub ' + mountpoint
+    subprocess.run(format_cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    pass
+
+
+@app.route('/workspace.html', methods=['POST', 'GET'])
+def workspace():
+    if request.method == "POST":
+        try:
+            if request.form['umount_disk']:
+                umount_disk()
+
+            if request.form['mount_disk']:
+                mount_disk()
+
+            if request.form['format_disk']:
+                format_disk()
+                return redirect('workspace.html')
+            else:
+                flash('Неккоректные данные !', category='error')
+                return render_template('authentication.html')
+
+        except Exception:
+            return render_template('workspace.html')
+
+
+
+    else:
+        return render_template('workspace.html')
+
+
+if __name__ == '__server__':
+    app.run(debug=True)
