dec3c0a64657229897942baef62769766be6f5df
romanTechDev <83161900+romanTechDev@users.noreply.github.com>
2021-08-08 21:11:01 +0300

    final 90% program ready

--- static/workspace.js
+++ static/workspace.js
@@ -2,8 +2,8 @@ Vue.component('mount_disk', {
 	props: {id:'id'},
 	template: `
 <td>
-	<button type="button" name="umount_disk {{id}}" class="btn btn-warning btn-sm">Отмонтировать</button>
-	<button type="button" name="format_disk {{id}}" class="btn btn-danger btn-sm">Форматировать</button>
+	<input type="submit" name="umount_disk" class="btn btn-warning btn-sm" value="Отмонтировать">
+	<input type="submit" name="format_disk" class="btn btn-danger btn-sm" value="Форматировать">
 </td>
 `
 })
@@ -15,32 +15,33 @@ Vue.component('umount_disk', {
 	props: {id:'id'},
 		template:`
 <td>
-	<button type="button" name="mount_disk {{id}}" class="btn btn-success btn-sm">Примонтировать</button>
+	<input type="submit" name="mount_disk" class="btn btn-success btn-sm" value="Примонтировать">
 	<input type="file" class="form-control-file" id="exampleFormControlFile1" webkitdirectory/>
+	<input type="text" name="path_to_mount" placeholder="Введите дирректорию !">
 </td>
 `
 })
 
-let all_input_disks_names =  document.getElementById("input_disks_names").value;
-let all_input_disks_types =  document.getElementById("input_disks_sizes").value;
-let all_input_disks_sizes =  document.getElementById("input_disks_types").value;
-let all_input_disks_mountpoints =  document.getElementById("input_disks_mountpoints").value;
-let all_input_disks_priorities =  document.getElementById("input_disks_priorities").value;
+let all_input_disks_names =  document.getElementById("input_disks_names").value;//  getElementsByName("input_disks_names")[0].valueOf();
+let all_input_disks_sizes =  document.getElementById("input_disks_sizes").value;//document.getElementsByName("input_disks_types").values().toString();
+let all_input_disks_mountpoints = document.getElementById("input_disks_mountpoints").value; //document.getElementsByName("input_disks_mountpoints").values().toString();
+let all_input_disks_types =  document.getElementById("input_disks_types").value;//document.getElementsByName("input_disks_sizes").values().toString();
+let all_input_disks_priorities =  document.getElementById("input_disks_priorities").value;//document.getElementsByName("input_disks_priorities").values().toString();
 
+//alert(all_input_disks_mountpoints)
 
 let id=0;
-let disks_names = all_input_disks_names.split(' ')//['sda','sdb','sdc','sdd'];
-let disks_sizes = all_input_disks_names.split(' ')//['20gb','1gb','50gb','100gb'];
-let disks_mountpoints = all_input_disks_names.split(' ')//['/','','/home/desktop',''];
-let disks_types = all_input_disks_names.split(' ')//['disk','part','disk','disk'];
-let disks_priority = all_input_disks_names.split(' ')//['system','system','common','common'];
+let disks_names = all_input_disks_names.split(' ');//['sda','sdb','sdc','sdd'];
+let disks_sizes = all_input_disks_sizes.split(' ');//['20gb','1gb','50gb','100gb'];
+let disks_mountpoints = all_input_disks_mountpoints.split(' ');//['/','','/home/desktop',''];
+let disks_types = all_input_disks_types.split(' ');//['disk','part','disk','disk'];
+let disks_priority = all_input_disks_priorities.split(' ');//['system','system','common','common'];
 
-
-
+//alert(disks_mountpoints)
 
 var app = new Vue({
 	el:'#app',
-
+	delimiters:['{[', ']}'],
 	data: {
 		array_disks:
 			{name:disks_names, size: disks_sizes, mountpoint: disks_mountpoints,type: disks_types, priority: disks_priority},
@@ -71,3 +72,7 @@ methods: {
 		}
 	}
 });
+
+
+
+//alert(disks_names + " " + disks_sizes + " " + disks_mountpoints + " " + disks_types + " " +disks_priority);
